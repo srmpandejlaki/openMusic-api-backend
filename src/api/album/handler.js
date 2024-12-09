2f49893be182126db31b12985037ffd37ac1dd85
Mutiara <mutiarapandejlaki27@gmail.com>
2024-12-09 11:03:04 +0800

    tambah .bind

--- src/api/album/handler.js
+++ src/api/album/handler.js
@@ -7,11 +7,11 @@ class AlbumHandler {
     this._service = service;
     this._validator = validator;
 
-    this.postAlbumHandler = this.postAlbumHandler(this);
-    this.getAlbumHandler = this.getAlbumHandler(this);
-    this.getAlbumByIdHandler = this.getAlbumByIdHandler(this);
-    this.putAlbumByIdHandler = this.putAlbumByIdHandler(this);
-    this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler(this);
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+    this.getAlbumHandler = this.getAlbumHandler.bind(this);
+    this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
+    this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
+    this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
 
     autoBind(this);
   }
--- src/api/song/handler.js
+++ src/api/song/handler.js
@@ -7,11 +7,11 @@ class SongHandler {
     this._service = service;
     this._validator = validator;
 
-    this.postSongHandler = this.postSongHandler(this);
-    this.getSongHandler = this.getSongHandler(this);
-    this.getSongByIdHandler = this.getSongByIdHandler(this);
-    this.putSongByIdHandler = this.putSongByIdHandler(this);
-    this.deleteSongByIdHandler = this.deleteSongByIdHandler(this);
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongHandler = this.getSongHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
 
     autoBind(this);
   }
